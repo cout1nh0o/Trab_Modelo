d00a1113c038b1b0d12956d9d1d1e25d34dd6ab4
Matheus Coutinho <coutinho_matheus@id.uff.br>
2024-11-24 22:50:11 -0300

    função modelo() para realizar os principais calculos e gráficos automaticamente; script mrls_2 feito por Grugg

--- mrls_1.R
+++ mrls_1.R
@@ -16,134 +16,214 @@ mar <- MARANHAO  %>%
             md_so2= mean(so2_ugm3,na.rm=TRUE),
             focos_queimada=sum(focos_queimada,na.rm = TRUE))
 
-#temperatura X umidade
-ggplot(mar, 
-       aes(x = max_temp, 
-           y = min_umid)) +
-  geom_point(color="cornflowerblue", 
-             size = 2, 
-             alpha = .6) +
-  geom_smooth(method = "lm",
-              color = "tomato",
-              se = FALSE) +
-  labs(x = "Temperatura",
-       y = "Umidade") +
-theme_classic()
+modelo <- function(tipo = "max", dt_min, dt_max){
+  
+  if(tipo == "media"){
+    mar <- MARANHAO  %>%   
+      mutate(DT_INTER = as.Date(datahora, format="%Y-%m-%d"))%>%
+      select(-c(mun_geocod,mun_nome,datahora,mun_uf_nome))%>%
+      filter(DT_INTER >= dt_min &  DT_INTER <= dt_max)%>%
+      group_by(DT_INTER)%>%
+      summarise(temp = mean(temperatura_c,na.rm = TRUE), 
+                umid = mean(umidade_relativa_percentual,na.rm = TRUE),
+                max_pm2.5 = max(pm25_ugm3,na.rm = TRUE),
+                md_co= mean(co_ppb,na.rm=TRUE),
+                md_no2= mean(no2_ppb,na.rm=TRUE),
+                md_o3= mean(o3_ppb,na.rm=TRUE),
+                md_so2= mean(so2_ugm3,na.rm=TRUE),
+                focos_queimada=sum(focos_queimada,na.rm = TRUE))
+  }else if ( tipo == 'mediana'){
+    mar <- MARANHAO  %>%   
+      mutate(DT_INTER = as.Date(datahora, format="%Y-%m-%d"))%>%
+      select(-c(mun_geocod,mun_nome,datahora,mun_uf_nome))%>%
+      filter(DT_INTER >= dt_min &  DT_INTER <= dt_max)%>%
+      group_by(DT_INTER)%>%
+      summarise(temp = median(temperatura_c,na.rm = TRUE), 
+                umid = median(umidade_relativa_percentual,na.rm = TRUE),
+                max_pm2.5 = max(pm25_ugm3,na.rm = TRUE),
+                md_co= mean(co_ppb,na.rm=TRUE),
+                md_no2= mean(no2_ppb,na.rm=TRUE),
+                md_o3= mean(o3_ppb,na.rm=TRUE),
+                md_so2= mean(so2_ugm3,na.rm=TRUE),
+                focos_queimada=sum(focos_queimada,na.rm = TRUE))
+  }else{
+    mar <- MARANHAO  %>%   
+      mutate(DT_INTER = as.Date(datahora, format="%Y-%m-%d"))%>%
+      select(-c(mun_geocod,mun_nome,datahora,mun_uf_nome))%>%
+      filter(DT_INTER >= dt_min &  DT_INTER <= dt_max)%>%
+      group_by(DT_INTER)%>%
+      summarise(temp = max(temperatura_c,na.rm = TRUE), 
+                umid = min(umidade_relativa_percentual,na.rm = TRUE),
+                max_pm2.5 = max(pm25_ugm3,na.rm = TRUE),
+                md_co= mean(co_ppb,na.rm=TRUE),
+                md_no2= mean(no2_ppb,na.rm=TRUE),
+                md_o3= mean(o3_ppb,na.rm=TRUE),
+                md_so2= mean(so2_ugm3,na.rm=TRUE),
+                focos_queimada=sum(focos_queimada,na.rm = TRUE))
+  }
+  
+  
+  #temperatura X umidade
+ dispersao <- ggplot(mar, 
+         aes(x = temp, 
+             y = umid)) +
+    geom_point(color="#1b6ca8", 
+               size = 2, 
+               alpha = .6) +
+    geom_smooth(method = "lm",
+                color = "#3aaf85",
+                se = FALSE) +
+    labs(x = "Temperatura",
+         y = "Umidade") +
+    theme_classic()
+ 
 
+ 
+ y <- mar$umid
+ x <- mar$temp
+ n <- length(y)
+ modelo <- lm(y ~ x, data = mar)
+ 
+ b0chap <- coef(modelo)[1]
+ ###B0 estimado = 159.8799 
+ 
+ b1chap <- coef(modelo)[2]
+ ###B1 estimado = -2.324915  
+ 
+ ### calculando y_ajustado
+ y_ajustado <- predict(modelo, mar)
+ 
+ ### estimado sigma^2
+ sigma2chapeu <- summary(modelo)$sigma^2
+ 
+ 
+ ### somatórios
+ sxy <- sum((x - mean(x)) * (y - mean(y)))
+ sxx <- sum((x - mean(x))^2)
+ syy <- sum((y - mean(y))^2)
+ 
+ 
+ 
+ ### IC de 95% de confiança para B0 
+ ICb0 <- confint(modelo)[1,]
+ 
+ ### IC de 95% de confiança para B0 
+ ICb1 <- confint(modelo)[2,]
+ 
+ ### IC para y_ajustado
+ 
+ # Grafico de dispersao com reta ajustada e intervalos de confianc
 
-#### Temperatura por umidade parece ter uma relação linear negativa e bem ajustada. 
-#### Dentre todos gráficos que Amanda mandou, esse parece ser o mais interessante.
-
-y <- mar$min_umid
-x <- mar$max_temp
-modelo <- lm(min_umid ~ max_temp, data = mar)
-
-b0chap <- coef(modelo)[1]
-###B0 estimado = 159.8799 
-
-b1chap <- coef(modelo)[2]
-###B1 estimado = -2.324915  
-
-### calculando y_ajustado
-y_ajustado <- predict(modelo, mar)
-
-### estimado sigma^2
-sigma2chapeu <- summary(modelo)$sigma^2
-# ou 
-sum((mar$min_umid - y_ajustado)^2)/363
-
-### somatórios
-sxy <- sum((x - mean(x)) * (y - mean(y)))
-sxx <- sum((x - mean(x))^2)
-syy <- sum((y - mean(y))^2)
-
-### IC de 95% de confiança para B0 
-ICb0 <- confint(modelo)[1,]
-
-### IC de 95% de confiança para B0 
-ICb1 <- confint(modelo)[2,]
-
-### IC para y_ajustado
-ICy_i_c <-  predict(modelo, interval = "confidence")
-ICy_i_c[,2:3]
-###### Teste de hipótese para B0
-tb0 <- b0chap/sqrt(sigma2chapeu * (1/length(x) + mean(x)^2/sxx))
-### p-valor para B0
-#p0 <- 2*pt(-abs(tb0), df = summary(modelo)$df[1])
-p01 <- 2 * (1 - pt(abs(tb0), df=summary(modelo)$df[2]))
-p01<0.005
-t <- dt(0.975, 363)
-
-tb0 > t
-### Rejeita h0, logo há evidencias para acreditar que b0 é diferente de 0 
-
-##### Teste de hipótese para B1
-t_b1 <- b1chap/sqrt(sigma2chapeu/sxx)
-### p-valor para B1
+ ###### Teste de hipótese para B0
+ tb0 <- b0chap/sqrt(sigma2chapeu * (1/length(x) + mean(x)^2/sxx))
+ ### p-valor para B0
+ #p0 <- 2*pt(-abs(tb0), df = summary(modelo)$df[1])
+ p01 <- 2 * (1 - pt(abs(tb0), df=summary(modelo)$df[2]))
+ p01<0.005
+ t <- dt(0.975, n-2)
+ 
+ tb0 > t
+ ### Rejeita h0, logo há evidencias para acreditar que b0 é diferente de 0 
+ 
+ ##### Teste de hipótese para B1
+ t_b1 <- b1chap/sqrt(sigma2chapeu/sxx)
+ ### p-valor para B1
  # p1 <- 2*pt(-abs(t_b1), df = summary(modelo)$df[1])
-  p12 <- 2 * (1 - pt(abs(t_b1),df = summary(modelo)$df[2] ))
-t <- dt(0.975, 363)
+ p12 <- 2 * (1 - pt(abs(t_b1),df = summary(modelo)$df[2] ))
+ t <- dt(0.975, n-2)
+ 
+ abs(t_b1) > t
+ 
+ p12 < 0.005
+ #Em geral vamos usar o pvalor deste teste para tomar a decis˜ao. Ent˜ao H0 ser´a rejeitada
+ #se o p-valor do teste for pequeno e aceita caso contr´ario
+ ### Pelo p-valor rejeita h0, mas pela estatistica de teste rejeita h0, logo há evidencias para acreditar que b0 é diferente de 0 
+ ICy_i_c <-  predict(modelo, interval = "confidence")
+ ICy_i_c[,2:3]
+ 
+ dispersao_ic <- ggplot(mar, aes(x = x, y = y)) +
+   geom_point(color="#1b6ca8") +  # Adiciona os pontos do gráfico de dispersão
+   geom_smooth(method = "lm", color = "#3aaf85", fill = "darkgray", alpha = 0.2) +  # Adiciona a reta de regressão e o intervalo de confiança
+   labs(title = "Gráfico de Dispersão com Intervalos de Confiança para as médias",
+        x = "Temperatura",
+        y = "Umidade") +
+   theme_minimal()
+ 
+ ##
+ ##### ANOVA 
+ 
+ tabAnova <- anova(modelo)
+ f0 <- tabAnova$`F value`[1]
+ f <- qf(0.95, 1, n-2)
+ f0 > f
+ 
+ residuos <-  y - modelo$fitted.values #residuals(ajuste)
+ 
+ shapiro.test(residuos)
+ 
+ ks.test(residuos,"pnorm",0, sqrt(sigma2chapeu))
+ 
+ ks.test(residuos,"pnorm",mean(residuos), sd(residuos))
+ 
+ gg_qqplot <- performance::check_model(modelo, check ='qq')
+ 
+gg_hist_residuos <-  ggplot(data = data.frame(x = residuos), aes(x)) +
+   geom_histogram(aes(y = ..density..),bins = 15, color = "gray", fill = "#1b6ca8") +
+   stat_function(fun = dnorm, args = list(mean = 0, sd = sqrt(sigma2chapeu)),
+                 color = "#3aaf85", size = 1) +
+   labs(title = "Histograma dos resíduos com Curva Normal",
+        x = "Valores",
+        y = "Frequência") +
+   theme_minimal()
+ 
+ gg_linear <- performance::check_model(modelo, check ='linearity')
+ gg_homo <- performance::check_model(modelo, check ='homogeneity')
+ 
+  retorno <- list(gg_dispersao = dispersao,
+                  correlacao = cor(x,y),
+                 modelo = modelo,
+                 b0chap = b0chap,
+                 b1chap = b1chap,
+                 ICb0 = ICb0,
+                 test_b0 = tb0,
+                 test_b1 = t_b1,
+                 ICb1 = ICb1,
+                 gg_dispersao_ic = dispersao_ic,
+                 tabela_Anova = tabAnova,
+                 sp = shapiro.test(residuos),
+                 ks1 = ks.test(residuos,"pnorm",0, sqrt(sigma2chapeu)),
+                 ks2 = ks.test(residuos,"pnorm",mean(residuos), sd(residuos)),
+                 gg_qqplot = gg_qqplot,
+                 gg_histogram_residuo = gg_hist_residuos,
+                 gg_dispersao_residuo = gg_linear,
+                 gg_homocestacieda = gg_homo,
+                 r2 = summary(modelo)$r.squared
+                 )
+  
+  return(retorno)
+}
 
-abs(t_b1) > t
 
-p12 < 0.005
-#Em geral vamos usar o pvalor deste teste para tomar a decis˜ao. Ent˜ao H0 ser´a rejeitada
-#se o p-valor do teste for pequeno e aceita caso contr´ario
-### Pelo p-valor rejeita h0, mas pela estatistica de teste rejeita h0, logo há evidencias para acreditar que b0 é diferente de 0 
+model <- modelo(dt_min = "2018-01-01", dt_max = "2018-12-31")
 
 
-
-###### 
-
-# Intervalo de predicao para y_novo
-#usando uma funcao do R
-xnovo <-  x
-pred_y <-  predict(modelo, newdata=mar, interval = "prediction", level = gama)
-
-
-# Gráfico de dispersão com reta ajustada e intervalos de confiança
-plot(x, y, main = "Grafico de Dispersao com Intervalos de Confianca",
-     xlab = "x", ylab = "y", bty="n", ylim=c(min(y, pred_y), max(y, pred_y)))
-ordem = order(x)
-# Adicionando o sombreado dos intervalos de confianca de y
-polygon(c(x[ordem], rev(x[ordem])), 
-        c(pred_y[ordem,2], rev(pred_y[ordem,3])), 
-        col = "darkgray", border = NA)
-# Adicionando o sombreado dos intervalos de confianca de mu
-polygon(c(x[ordem], rev(x[ordem])), 
-        c(ICy_i_c[ordem,2], rev(ICy_i_c[ordem,3])), 
-        col = "blue", border = NA)
-#adicionando a reta ajustada
-abline(ajuste, col = "red", lwd = 2)
-#adicionando os valores observados
-points(x,y)
-
-
-
-##### ANOVA 
-
-tabAnova <- anova(modelo)
-
-
-residuos <-  y - modelo$fitted.values #residuals(ajuste)
-plot(round(residuos-modelo$residuals,9))
-
-
-hist(residuos, freq=F, main="Histograma", ylab="densidade")
-curve(dnorm(x, 0, sqrt(sigma2chapeu)), add=T, lwd=2, col="red")
-
-qqnorm(residuos, xlab="quatis teóricos", ylab="quantis amostrais", bty="n")
-qqline(residuos, col="red", lwd=2)
-
-shapiro.test(residuos)
-
-ks.test(residuos,"pnorm",0, sqrt(sigma2chapeu))
-
-ks.test(residuos,"pnorm",mean(residuos), sd(residuos))
-
-par(mfrow=c(2,2))
-plot(x, residuos)
-plot(y, residuos)
-plot(residuos)
-
-### o grafico de y com os residos me pareceu estranho. 
+model$gg_dispersao
+model$correlacao
+model$b0chap
+model$b1chap
+model$ICb0
+model$ICb1
+model$test_b0
+model$test_b0>dt(0.975, 363)
+model$test_b1
+abs(model$test_b1)>dt(0.975, 363)
+model$gg_dispersao_ic
+model$tabela_Anova
+model$sp
+model$ks1
+model$ks2
+model$gg_qqplot
+model$gg_histogram_residuo
+model$gg_dispersao_residuo
+model$gg_homocestacieda
+model$r2
---
+++ mrls_2.R
@@ -0,0 +1,238 @@
+library(tidyverse)
+library(patchwork)
+
+
+
+# === Passos prévios ====
+
+MARANHAO <- readxl::read_excel("MARANHAO.xlsx")
+
+mar <- MARANHAO  %>%
+  mutate(DT_INTER = as.Date(datahora, format = "%Y-%m-%d")) %>%
+  select(-c(mun_geocod, mun_nome, datahora, mun_uf_nome)) %>%
+  filter(DT_INTER >= "2018-01-01" &  DT_INTER <= '2018-12-31') %>%
+  group_by(DT_INTER) %>%
+  summarise(
+    mean_temp = mean(temperatura_c, na.rm = TRUE),
+    mean_umid = mean(umidade_relativa_percentual, na.rm = TRUE),
+    median_temp = median(temperatura_c, na.rm = TRUE),
+    median_umid = median(umidade_relativa_percentual, na.rm = TRUE),
+    mean_pm2.5 = mean(pm25_ugm3, na.rm = TRUE),
+    mean_co = mean(co_ppb, na.rm = TRUE),
+    mean_no2 = mean(no2_ppb, na.rm = TRUE),
+    mean_o3 = mean(o3_ppb, na.rm = TRUE),
+    mean_so2 = mean(so2_ugm3, na.rm = TRUE),
+    focos_queimada = sum(focos_queimada, na.rm = TRUE)
+  )
+
+
+
+# ==== Análise descritiva ====
+
+# Histograma baseado na densidade para mean_umid
+ggplot(mar, aes(x = mean_umid)) +
+  geom_histogram(aes(y = ..density..),
+                 binwidth = 1,
+                 fill = "cornflowerblue",
+                 color = "black",
+                 alpha = .6) +
+  labs(x = "Umidade", y = "Densidade") +
+  scale_x_continuous(limits = c(85, 100), 
+                     breaks = seq(85, 100, by = 5)) +
+  theme_minimal() -> h1
+
+
+# Histograma baseado na densidade para mean_temp
+ggplot(mar, aes(x = mean_temp)) +
+  geom_histogram(aes(y = ..density..),
+                 binwidth = 1,
+                 fill = "tomato",
+                 color = "black",
+                 alpha = .6) +
+  labs(x = "Temperatura", y = "Densidade") +
+  scale_x_continuous(limits = c(20, 35), 
+                     breaks = seq(20, 35, by = 5)) +
+  theme_minimal() -> h2
+
+# Plotando um abaixo do outro (o patchwork precisa estar carregado)
+h1 / h2
+
+# Teste de normalidade para a variável resposta
+shapiro.test(mar$mean_temp) # Não-normal
+ks.test(mar$mean_temp, "pnorm", mean(mar$mean_temp), sd(mar$mean_temp)) # Normal
+nortest::ad.test(mar$mean_temp) # Não-normal
+nortest::lillie.test(mar$mean_temp) # Não-normal
+tseries::jarque.bera.test(mar$mean_temp) # Não-normal
+
+
+# Dispersão da temperatura por umidade
+ggplot(mar, aes(x = mean_umid, y = mean_temp)) +
+  geom_point(color = "cornflowerblue",
+             size = 2,
+             alpha = .6) +
+  geom_smooth(method = "lm",
+              color = "tomato",
+              se = FALSE) +
+  labs(x = "Umidade", y = "Temperatura") +
+  scale_y_continuous(limits = c(20, 35),
+                     breaks = seq(20, 35, by = 5)) +
+  scale_x_continuous(limits = c(85, 100),
+                     breaks = seq(85, 100, by = 5)) +
+  theme_minimal() -> d1
+d1
+
+# Para plotar junto
+# par(mfrow = c(2, 2))
+
+# Série temporal da temperatura média
+mt <- ggplot(mar, aes(x = DT_INTER, y = mean_temp)) +
+  geom_line(color = "steelblue", size = .5) +
+  geom_point(color = "cornflowerblue", size = 1) +
+  labs(title = "Evolução da média da temperatura no tempo",
+       x = "Data", 
+       y = "Temperatura média") +
+  scale_x_date(date_labels = "%b %Y",
+               date_breaks = "1 month") +
+  theme_minimal()
+
+# Série temporal da umidade média
+mu <- ggplot(mar, aes(x = DT_INTER, y = mean_umid)) +
+  geom_line(color = "steelblue", size = .5) +
+  geom_point(color = "cornflowerblue", size = 1) +
+  labs(title = "Evolução da média da umidade no tempo",
+       x = "Data", 
+       y = "Umidade média") +
+  scale_x_date(date_labels = "%b %Y",
+               date_breaks = "1 month") +
+  theme_minimal()
+
+# Plotando um abaixo do outro (lembrar de carregar o patchwork)
+mt / mu
+
+
+# Correlação entre temperatura e umidade
+cor(x = mar$mean_temp, y = mar$mean_umid) # -0.8504322
+
+
+# Achados:
+
+# 1. Há uma correlação linear negativa da ordem de 85%, indicando que uma modelagem
+# linear entre essas duas variáveis é uma boa opção.
+# 2. Essa relação pode ser vista graficamente, tanto na bi-dispersão, quanto na
+# evolução temporal, onde se torna nítido o comportamento antagonista entre as
+# variáveis.
+# 3. O uso da mediana em vez da média não se mostrou interessante, pois ela não
+# gera dados contínuos, apesar de acusar mais normalidade que a média (considerando
+# o Shapiro-Wilk). A menos do teste de Kolmogorov-Smirnov, todos os outros
+# recusaram normalidade da temperatura média. Assim, torna-se útil e inteligente
+# utilizar o resultado do teste de K.S.
+
+
+
+# === Modelo linear simples ====
+
+# Modelagem
+x <- mar$mean_umid
+y <- mar$mean_temp
+
+modelo <- lm(y ~ x, data = mar)
+
+# Estatísticas e outras informações
+summary(modelo)
+performance::check_model(modelo, check ='qq')
+performance::check_model(modelo, check ='homogeneity')
+performance::check_model(modelo, check ='linearity')
+# Diagnóstico visual
+ performance::check_model(modelo, FALSE) # Lembrar de aumentar a região de plotagem
+
+# Normalidade dos resíduos
+ks.test(modelo$residuals, "pnorm", mean(modelo$residuals), sd(modelo$residuals)) # Normal
+
+# Independência dos resíduos
+lmtest::dwtest(modelo)
+
+# Homocedasticidade
+lmtest::bptest(modelo)
+
+# anova(modelo) # Já sai no summary do modelo
+
+# Coeficientes
+coefficients(modelo)
+confint.lm(modelo)
+
+# Valores ajustados
+ajustados <- modelo$fitted.values
+ajustados_IC <- predict(modelo, interval = "confidence") # Pegar colunas 2 e 3
+
+
+
+# Daqui pra baixo tem que avaliar a necessidade
+
+### estimado sigma^2
+(sigma2chapeu <- summary(modelo)$sigma ^ 2)
+
+
+# Intervalo de predicao para y_novo
+#usando uma funcao do R
+
+(xnovo <-  x)
+(pred_y <-  predict(modelo,
+                    newdata = mar,
+                    interval = "prediction",
+                    level = gama))
+
+
+# Gráfico de dispersão com reta ajustada e intervalos de confiança
+plot(
+  x,
+  y,
+  main = "Grafico de Dispersao com Intervalos de Confianca",
+  xlab = "x",
+  ylab = "y",
+  bty = "n",
+  ylim = c(min(y, pred_y), max(y, pred_y))
+)
+ordem = order(x)
+# Adicionando o sombreado dos intervalos de confianca de y
+polygon(c(x[ordem], rev(x[ordem])),
+        c(pred_y[ordem, 2], rev(pred_y[ordem, 3])),
+        col = "darkgray",
+        border = NA)
+# Adicionando o sombreado dos intervalos de confianca de mu
+polygon(c(x[ordem], rev(x[ordem])),
+        c(ICy_i_c[ordem, 2], rev(ICy_i_c[ordem, 3])),
+        col = "blue",
+        border = NA)
+#adicionando a reta ajustada
+abline(modelo, col = "red", lwd = 2)
+#adicionando os valores observados
+points(x, y)
+
+
+hist(residuos,
+     freq = F,
+     main = "Histograma",
+     ylab = "densidade")
+curve(dnorm(x, 0, sqrt(sigma2chapeu)),
+      add = T,
+      lwd = 2,
+      col = "red")
+
+qqnorm(residuos,
+       xlab = "quatis teóricos",
+       ylab = "quantis amostrais",
+       bty = "n")
+qqline(residuos, col = "red", lwd = 2)
+
+shapiro.test(residuos)
+
+ks.test(residuos, "pnorm", 0, sqrt(sigma2chapeu))
+
+ks.test(residuos, "pnorm", mean(residuos), sd(residuos))
+
+par(mfrow = c(2, 2))
+plot(x, residuos)
+plot(y, residuos)
+plot(residuos)
+
+### o grafico de y com os residos me pareceu estranho.
